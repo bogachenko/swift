07a3f73e0e42986d3747e8dc4793b9268d976d68
bogachenko <bogachenkove@gmail.com>
2025-04-27 20:33:45 +0300

    Update TransferSWIFT.sol
    * Set rateLimit to 1 minute.
    * Disabled token acceptance (ETH native coin only).
    * Check that only a contract can be added to the whitelist.
    * Added gas limits for each standard and a global limit.
    * Simplified setMaxRecipients, without selecting the amount.
    * Disabled adding the owner to the blacklist.
    * Disabled sending to zero address.
    * Added ERC165 support.
    * Events added for all operations.

--- transfer-swift/ethereum/contracts/TransferSWIFT.sol
+++ transfer-swift/ethereum/contracts/TransferSWIFT.sol
@@ -12,9 +12,12 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 
-contract TransferSWIFT is Ownable, Pausable, ReentrancyGuard {
+contract TransferSWIFT is Ownable, Pausable, ReentrancyGuard, IERC165 {
     using SafeERC20 for IERC20;
+    using Address for address;
 
     string public constant name = "TransferSWIFT";
     string public constant symbol = "SWIFT";
@@ -23,10 +26,10 @@ contract TransferSWIFT is Ownable, Pausable, ReentrancyGuard {
     uint256 public taxFee = CheckTaxFee;
 
     uint256 public defaultMaxRecipients = 15;
-    mapping(address => uint256) public maxRecipientsOverride;
+    mapping(address => bool) public maxRecipientsOverride;
 
     mapping(address => uint256) public lastUsed;
-    uint256 public rateLimitInterval = 10 minutes;
+    uint256 public rateLimitInterval = 1 minutes;
 
     mapping(address => bool) public blacklist;
     mapping(address => bool) public whitelistERC20;
@@ -34,6 +37,12 @@ contract TransferSWIFT is Ownable, Pausable, ReentrancyGuard {
     mapping(address => bool) public whitelistERC1155;
 
     mapping(bytes32 => bool) private usedNonces;
+
+    uint256 public constant gasLimitGlobal = 30000000;
+    uint256 public gasLimitEth = 26000;
+    uint256 public gasLimitErc20 = 70000;
+    uint256 public gasLimitErc721 = 105000;
+    uint256 public gasLimitErc1155 = 72000;
 
     event MultiTransfer(
         address indexed sender,
@@ -44,7 +53,7 @@ contract TransferSWIFT is Ownable, Pausable, ReentrancyGuard {
         bytes32 nonce
     );
     event TaxFeeChanged(uint256 oldFee, uint256 newFee);
-    event MaxRecipientsChanged(address indexed account, uint256 oldMax, uint256 newMax);
+    event MaxRecipientsGranted(address indexed account);
     event RateLimitIntervalChanged(uint256 oldInterval, uint256 newInterval);
     event BlacklistAdded(address indexed account);
     event BlacklistRemoved(address indexed account);
@@ -56,14 +65,21 @@ contract TransferSWIFT is Ownable, Pausable, ReentrancyGuard {
     event WhitelistERC1155Removed(address indexed token);
     event Rescue(address indexed to, uint256 amount);
     event NonceUsed(bytes32 indexed nonce);
+    event GasLimitTooHigh(string limitType);
+    event OnlyPausedWithdrawAllowed();
 
     modifier notBlacklisted(address account) {
         require(!blacklist[account], "Address blacklisted");
         _;
     }
 
-    constructor() payable Ownable(msg.sender) {}
+    constructor() payable {}
 
+    function supportsInterface(
+        bytes4 interfaceId
+    ) external pure override returns (bool) {
+        return interfaceId == type(IERC165).interfaceId;
+    }
     // Owner functions
     function setTaxFee(uint256 _newFee) external onlyOwner {
         require(_newFee >= CheckTaxFee, "Fee too low");
@@ -71,10 +87,9 @@ contract TransferSWIFT is Ownable, Pausable, ReentrancyGuard {
         taxFee = _newFee;
     }
 
-    function setMaxRecipients(address account, uint256 maxCount) external onlyOwner {
-        require(maxCount <= 20, "Max 20 recipients");
-        emit MaxRecipientsChanged(account, maxRecipientsOverride[account], maxCount);
-        maxRecipientsOverride[account] = maxCount;
+    function grantMaxRecipients(address account) external onlyOwner {
+        maxRecipientsOverride[account] = true;
+        emit MaxRecipientsGranted(account);
     }
 
     function setRateLimitInterval(uint256 _interval) external onlyOwner {
@@ -91,6 +106,7 @@ contract TransferSWIFT is Ownable, Pausable, ReentrancyGuard {
     }
 
     function addBlacklist(address account) external onlyOwner {
+        require(account != owner(), "Cannot blacklist owner");
         blacklist[account] = true;
         emit BlacklistAdded(account);
     }
@@ -101,6 +117,7 @@ contract TransferSWIFT is Ownable, Pausable, ReentrancyGuard {
     }
 
     function addWhitelistERC20(address token) external onlyOwner {
+        require(token.isContract(), "Must be contract");
         whitelistERC20[token] = true;
         emit WhitelistERC20Added(token);
     }
@@ -111,6 +128,7 @@ contract TransferSWIFT is Ownable, Pausable, ReentrancyGuard {
     }
 
     function addWhitelistERC721(address token) external onlyOwner {
+        require(token.isContract(), "Must be contract");
         whitelistERC721[token] = true;
         emit WhitelistERC721Added(token);
     }
@@ -121,6 +139,7 @@ contract TransferSWIFT is Ownable, Pausable, ReentrancyGuard {
     }
 
     function addWhitelistERC1155(address token) external onlyOwner {
+        require(token.isContract(), "Must be contract");
         whitelistERC1155[token] = true;
         emit WhitelistERC1155Added(token);
     }
@@ -132,8 +151,10 @@ contract TransferSWIFT is Ownable, Pausable, ReentrancyGuard {
 
     // Rescue ETH when paused
     function rescueETH(address payable to) external onlyOwner whenPaused {
+        require(to != address(0), "Zero address");
         uint256 bal = address(this).balance;
         emit Rescue(to, bal);
+        emit OnlyPausedWithdrawAllowed();
         to.transfer(bal);
     }
 
@@ -159,7 +180,10 @@ contract TransferSWIFT is Ownable, Pausable, ReentrancyGuard {
         bytes32 nonce
     ) external payable nonReentrant whenNotPaused notBlacklisted(msg.sender) {
         // Rate limit
-        require(block.timestamp >= lastUsed[msg.sender] + rateLimitInterval, "Rate limit");
+        require(
+            block.timestamp >= lastUsed[msg.sender] + rateLimitInterval,
+            "Rate limit"
+        );
         lastUsed[msg.sender] = block.timestamp;
 
         // Nonce check
@@ -168,79 +192,99 @@ contract TransferSWIFT is Ownable, Pausable, ReentrancyGuard {
         emit NonceUsed(nonce);
 
         // Check recipients count
-        uint256 maxR = maxRecipientsOverride[msg.sender] > 0 ? maxRecipientsOverride[msg.sender] : defaultMaxRecipients;
+        uint256 totalOps = ethRecipients.length +
+            erc20Recipients.length +
+            erc721Recipients.length +
+            erc1155Recipients.length;
+        if (gasleft() > gasLimitGlobal) {
+            emit GasLimitTooHigh("global");
+            revert("Global gas limit too high");
+        }
+
+        uint256 maxR = maxRecipientsOverride[msg.sender]
+            ? 20
+            : defaultMaxRecipients;
         require(
-            ethRecipients.length <= maxR,
-            "Too many ETH recipients"
-        );
-        require(
-            erc20Recipients.length <= maxR,
-            "Too many ERC20 recipients"
-        );
-        require(
-            erc721Recipients.length <= maxR,
-            "Too many ERC721 recipients"
-        );
-        require(
-            erc1155Recipients.length <= maxR,
-            "Too many ERC1155 recipients"
+            ethRecipients.length <= maxR &&
+                erc20Recipients.length <= maxR &&
+                erc721Recipients.length <= maxR &&
+                erc1155Recipients.length <= maxR,
+            "Too many recipients"
         );
 
         // Collect needed ETH
         uint256 totalEth = 0;
         for (uint i = 0; i < ethAmounts.length; i++) {
+            require(ethRecipients[i] != address(0), "Zero address");
+            require(!blacklist[ethRecipients[i]], "Recipient blacklisted");
+            require(gasleft() >= gasLimitEth, "ETH gas limit");
             totalEth += ethAmounts[i];
         }
         require(msg.value == totalEth + taxFee, "Incorrect ETH sent");
 
         // Distribute ETH
         for (uint i = 0; i < ethRecipients.length; i++) {
-            address to = ethRecipients[i];
-            require(!blacklist[to], "Recipient blacklisted");
-            payable(to).transfer(ethAmounts[i]);
+            payable(ethRecipients[i]).transfer(ethAmounts[i]);
         }
 
         // ERC-20 transfers
         require(
             erc20Tokens.length == erc20Recipients.length &&
-            erc20Recipients.length == erc20Amounts.length,
+                erc20Recipients.length == erc20Amounts.length,
             "ERC20 array mismatch"
         );
         for (uint i = 0; i < erc20Recipients.length; i++) {
-            address token = erc20Tokens[i];
-            require(whitelistERC20[token], "ERC20 not whitelisted");
-            address to = erc20Recipients[i];
-            require(!blacklist[to], "Recipient blacklisted");
-            IERC20(token).safeTransferFrom(msg.sender, to, erc20Amounts[i]);
+            require(gasleft() >= gasLimitErc20, "ERC20 gas limit");
+            require(erc20Recipients[i] != address(0), "Zero address");
+            require(!blacklist[erc20Recipients[i]], "Recipient blacklisted");
+            require(whitelistERC20[erc20Tokens[i]], "ERC20 not whitelisted");
+            IERC20(erc20Tokens[i]).safeTransferFrom(
+                msg.sender,
+                erc20Recipients[i],
+                erc20Amounts[i]
+            );
         }
 
         // ERC-721 transfers
         require(
             erc721Tokens.length == erc721Recipients.length &&
-            erc721Recipients.length == erc721Ids.length,
+                erc721Recipients.length == erc721Ids.length,
             "ERC721 array mismatch"
         );
         for (uint i = 0; i < erc721Recipients.length; i++) {
-            address token = erc721Tokens[i];
-            require(whitelistERC721[token], "ERC721 not whitelisted");
-            address to = erc721Recipients[i];
-            require(!blacklist[to], "Recipient blacklisted");
-            IERC721(token).safeTransferFrom(msg.sender, to, erc721Ids[i]);
+            require(gasleft() >= gasLimitErc721, "ERC721 gas limit");
+            require(erc721Recipients[i] != address(0), "Zero address");
+            require(!blacklist[erc721Recipients[i]], "Recipient blacklisted");
+            require(whitelistERC721[erc721Tokens[i]], "ERC721 not whitelisted");
+            IERC721(erc721Tokens[i]).safeTransferFrom(
+                msg.sender,
+                erc721Recipients[i],
+                erc721Ids[i]
+            );
         }
 
         // ERC-1155 transfers
         require(
             erc1155Tokens.length == erc1155Recipients.length &&
-            erc1155Recipients.length == erc1155Ids.length &&
-            erc1155Ids.length == erc1155Amounts.length,
+                erc1155Recipients.length == erc1155Ids.length &&
+                erc1155Ids.length == erc1155Amounts.length,
             "ERC1155 array mismatch"
         );
         for (uint i = 0; i < erc1155Recipients.length; i++) {
-            address token = erc1155Tokens[i];
-            require(whitelistERC1155[token], "ERC1155 not whitelisted");
-            address to = erc1155Recipients[i];
-            require(!blacklist[to], "Recipient blacklisted");
-            IERC1155(token).safeTransferFrom(msg.sender, to, erc1155Ids[i], erc1155Amounts[i], "");
+            require(gasleft() >= gasLimitErc1155, "ERC1155 gas limit");
+            require(erc1155Recipients[i] != address(0), "Zero address");
+            require(!blacklist[erc1155Recipients[i]], "Recipient blacklisted");
+            require(
+                whitelistERC1155[erc1155Tokens[i]],
+                "ERC1155 not whitelisted"
+            );
+            IERC1155(erc1155Tokens[i]).safeTransferFrom(
+                msg.sender,
+                erc1155Recipients[i],
+                erc1155Ids[i],
+                erc1155Amounts[i],
+                ""
+            );
         }
 
         emit MultiTransfer(
@@ -254,6 +298,11 @@ contract TransferSWIFT is Ownable, Pausable, ReentrancyGuard {
     }
 
     // Fallback reject other tokens
-    receive() external payable {}
-    fallback() external payable {}
+    receive() external payable {
+        require(msg.sender != address(this), "Reject contract token");
+    }
+
+    fallback() external payable {
+        require(msg.sender != address(this), "Reject contract token");
+    }
 }
