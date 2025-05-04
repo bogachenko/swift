0b4656fb9b9bc5124eae44ed6a22149e185483c4
bogachenko <bogachenkove@gmail.com>
2025-05-04 20:00:07 +0300

    Update TestCOIN.sol
    * Added support for the ERC20 standard

--- transfer-swift/ethereum/tests/ERC20/TestCOIN.sol
+++ transfer-swift/ethereum/tests/ERC20/TestCOIN.sol
@@ -4,9 +4,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/interfaces/IERC721Receiver.sol";
-import "@openzeppelin/contracts/interfaces/IERC1155Receiver.sol";
-contract TestCOIN is ERC20, ERC20Permit, ERC20Pausable, Ownable, IERC721Receiver, IERC1155Receiver {
+import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
+contract TestCOIN is ERC20, ERC20Permit, ERC20Pausable, Ownable {
     constructor()
         ERC20("TestCOIN", "tCOIN")
         ERC20Permit("TestCOIN")
@@ -21,9 +20,7 @@ contract TestCOIN is ERC20, ERC20Permit, ERC20Pausable, Ownable, IERC721Receiver
         _burn(msg.sender, amount);
     }
     function burnFrom(address account, uint256 amount) external {
-        uint256 currentAllowance = allowance(account, msg.sender);
-        require(currentAllowance >= amount, "Burn amount exceeds allowance");
-        _approve(account, msg.sender, currentAllowance - amount);
+        _spendAllowance(account, msg.sender, amount);
         _burn(account, amount);
     }
     function pause() external onlyOwner {
@@ -32,28 +29,17 @@ contract TestCOIN is ERC20, ERC20Permit, ERC20Pausable, Ownable, IERC721Receiver
     function unpause() external onlyOwner {
         _unpause();
     }
-    function _update(address from, address to, uint256 value) internal override(ERC20, ERC20Pausable) {
+    function supportsInterface(bytes4 interfaceId) public pure returns (bool) {
+        return 
+            interfaceId == type(IERC20).interfaceId ||
+            interfaceId == type(IERC165).interfaceId ||
+            interfaceId == type(IERC20Permit).interfaceId;
+    }
+    function _update(address from, address to, uint256 value)
+        internal
+        override(ERC20, ERC20Pausable)
+    {
         super._update(from, to, value);
-    }
-    receive() external payable {
-        revert("Contract does not accept ETH");
-    }
-    fallback() external payable {
-        revert("Contract does not accept fallback calls");
-    }
-    function onERC721Received(address, address, uint256, bytes calldata) external pure override returns (bytes4) {
-        revert("NFTs not accepted");
-    }
-    function onERC1155Received(address, address, uint256, uint256, bytes calldata) external pure override returns (bytes4) {
-        revert("ERC1155 not accepted");
-    }
-    function onERC1155BatchReceived(address, address, uint256[] calldata, uint256[] calldata, bytes calldata) external pure override returns (bytes4) {
-        revert("ERC1155 batch not accepted");
-    }
-    function supportsInterface(bytes4) public pure override returns (bool) {
-        return false;
-    }
-    function tokenFallback(address, uint256, bytes calldata) external pure {
-        revert("ERC20 not accepted");
+        require(!paused(), "ERC20Pausable: token transfer while paused");
     }
 }
