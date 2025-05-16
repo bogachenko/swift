aef0bd92103ac2cc6007c4037b667c6a0c710b5e
bogachenko <bogachenkove@gmail.com>
2025-05-16 19:22:40 +0300

    Update recipientlimit.test.js
    * Add a check function mutlitransfer.

--- SWIFT Protocol/ethereum/test/recipientlimit.test.js
+++ SWIFT Protocol/ethereum/test/recipientlimit.test.js
@@ -84,7 +84,102 @@ describe("SWIFT Protocol Recipient Limit Functions", function() {
 			expect(await swift.extendedRecipients(user2.address)).to.be.true;
 		});
 		it("should work with zero address", async function() {
-			await expect(swift.connect(admin1).setMaxRecipients(ethers.ZeroAddress)).to.be.reverted; // Should fail as zero address checks are in place
+			await expect(swift.connect(admin1).setMaxRecipients(ethers.ZeroAddress)).to.be.reverted;
+		});
+	});
+	describe("multiTransfer recipient limits", function() {
+		it("should allow transfers up to currentRecipients for standard users", async function() {
+			const currentLimit = await swift.currentRecipients();
+			const recipients = Array(Number(currentLimit)).fill().map(() => ethers.Wallet.createRandom().address);
+			const amount = ethers.parseEther("0.01");
+			const amounts = Array(Number(currentLimit)).fill(amount);
+			const totalValue = (amount + await swift.taxFee()) * currentLimit;
+			await expect(swift.connect(user1).multiTransfer(0,
+				ethers.ZeroAddress, recipients, amounts,
+				[], false, ethers.randomBytes(32), {
+					value: totalValue
+				})).to.not.be.reverted;
+		});
+		it("should reject transfers exceeding currentRecipients for standard users", async function() {
+			await ethers.provider.send("evm_increaseTime", [Number(await swift.rateLimitDuration()) + 1]);
+			await ethers.provider.send("evm_mine");
+			await swift.connect(admin1).setDefaultRecipients(user1.address);
+			expect(await swift.extendedRecipients(user1.address)).to.be.false;
+			const currentLimit = Number(await swift.currentRecipients());
+			const recipients = Array.from({
+				length: currentLimit + 1
+			}, () => ethers.Wallet.createRandom().address);
+			const amountPerRecipient = ethers.parseEther("0.01");
+			const taxFee = await swift.taxFee();
+			const totalValue = amountPerRecipient * BigInt(currentLimit + 1) + taxFee * BigInt(currentLimit + 1);
+			await expect(swift.connect(user1).multiTransfer(0, ethers.ZeroAddress, recipients, recipients.map(() => amountPerRecipient),
+				[], false, ethers.randomBytes(32), {
+					value: totalValue
+				})).to.be.revertedWith("Too many recipients");
+		});
+		it("should allow transfers up to maxRecipients for users with extended limit", async function() {
+			await ethers.provider.send("evm_increaseTime", [Number(await swift.rateLimitDuration()) + 1]);
+			await ethers.provider.send("evm_mine");
+			await swift.connect(admin1).setMaxRecipients(user1.address);
+			expect(await swift.extendedRecipients(user1.address)).to.be.true;
+			const maxLimit = Number(await swift.maxRecipients());
+			const amountPerRecipient = ethers.parseEther("0.01");
+			const taxFee = await swift.taxFee();
+			const recipients = Array.from({
+				length: maxLimit
+			}, () => ethers.Wallet.createRandom().address);
+			const amounts = recipients.map(() => amountPerRecipient);
+			const totalValue = amountPerRecipient * BigInt(maxLimit) + taxFee * BigInt(maxLimit);
+			await expect(swift.connect(user1).multiTransfer(0, ethers.ZeroAddress, recipients, amounts,
+				[], false, ethers.randomBytes(32), {
+					value: totalValue
+				})).to.not.be.reverted;
+		});
+		it("should reject transfers exceeding maxRecipients even for users with extended limit", async function() {
+			await ethers.provider.send("evm_increaseTime", [Number(await swift.rateLimitDuration()) + 1]);
+			await ethers.provider.send("evm_mine");
+			await swift.connect(admin1).setMaxRecipients(user1.address);
+			expect(await swift.extendedRecipients(user1.address)).to.be.true;
+			const maxLimit = Number(await swift.maxRecipients());
+			const amountPerRecipient = ethers.parseEther("0.01");
+			const taxFee = await swift.taxFee();
+			const recipients = Array.from({
+				length: maxLimit + 1
+			}, () => ethers.Wallet.createRandom().address);
+			const amounts = recipients.map(() => amountPerRecipient);
+			const totalValue = amountPerRecipient * BigInt(maxLimit + 1) + taxFee * BigInt(maxLimit + 1);
+			await swift.connect(admin1).setDefaultRecipients(user1.address);
+			expect(await swift.extendedRecipients(user1.address)).to.be.false;
+			await expect(swift.connect(user1).multiTransfer(0, ethers.ZeroAddress, recipients, amounts,
+				[], false, ethers.randomBytes(32), {
+					value: totalValue
+				})).to.be.revertedWith("Too many recipients");
+		});
+		it("should apply updated currentRecipients limit", async function() {
+			const newLimit = 20n;
+			await swift.connect(admin1).updateRecipientLimit(newLimit);
+			const recipients = Array.from({
+				length: Number(newLimit)
+			}, () => ethers.Wallet.createRandom().address);
+			const amounts = recipients.map(() => ethers.parseEther("0.01"));
+			const totalValue = ethers.parseEther("0.01") * newLimit + (await swift.taxFee()) * newLimit;
+			await expect(swift.connect(user2).multiTransfer(0, ethers.ZeroAddress, recipients, amounts,
+				[], false, ethers.randomBytes(32), {
+					value: totalValue
+				})).to.not.be.reverted;
+			const tooManyRecipients = Array.from({
+				length: Number(newLimit) + 1
+			}, () => ethers.Wallet.createRandom().address);
+			const tooManyAmounts = tooManyRecipients.map(() => ethers.parseEther("0.01"));
+			const taxFee = await swift.taxFee();
+			const tooManyValue = ethers.parseEther("0.01") * BigInt(tooManyRecipients.length) + taxFee * BigInt(tooManyRecipients.length);
+			await swift.connect(admin1).setDefaultRecipients(user2.address);
+			await ethers.provider.send("evm_increaseTime", [Number(await swift.rateLimitDuration()) + 1]);
+			await ethers.provider.send("evm_mine");
+			await expect(swift.connect(user2).multiTransfer(0, ethers.ZeroAddress, tooManyRecipients, tooManyAmounts,
+				[], false, ethers.randomBytes(32), {
+					value: tooManyValue
+				})).to.be.revertedWith("Too many recipients");
 		});
 	});
 });
